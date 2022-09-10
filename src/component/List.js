06ffae605e8e1de949940eed88adabf278c21ded
edwintsanghm <edwintsanghm@gmail.com>
2022-09-10 16:26:21 -0400

    ui updates for List Page

--- src/component/List.js
+++ src/component/List.js
@@ -1,5 +1,13 @@
 import { Link } from "react-router-dom";
 import React, { useState } from 'react';
+import { faker } from '@faker-js/faker';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+
 /*
     List in Driver View
     - Your Ride
@@ -8,22 +16,29 @@ import React, { useState } from 'react';
         
     List in Passenger View
 */
-const data = [{
-    profile:{
-        info: {name: "Edwin", gender:"gender", from: "Markham", To: "18 York Street", workday:[1,2,3,4,5], eta: "9:00 am"},
-        carInfo: {model: "model", make: "make", number: "CXR004", seats: 4, freeSeats: 4},
-    },
-    passengerList:[
-        {name: "Edwin", gender:"gender", from: "Markham", To: "18 York Street", workday:[1,2,3,4,5], eta: "9:00 am"},
-        {name: "Gordon", gender:"gender", from: "High Park", To: "18 York Street", workday:[1,2,3,4,5], eta: "9:00 am"},
-        {name: "Nonso", gender:"gender", from: "Scarborough Town Centre", To: "Don Mills Station", workday:[1,2,3,4,5], eta: "9:00 am"},
-        {name: "Char", gender:"gender", from: "Markham", To: "18 York Street", workday:[1,2,3,4,5], eta: "9:00 am"},
-    ]
-}
-    ];
 
-const List = () => {
+const availableRides = [
+    {
+        info: {avatarUrl: faker.image.avatar(), name: faker.name.firstName(), gender:"gender", from: "Markham", to: "18 York Street", workday:[1,2,3,4,5], eta: "9:00 am", rating: 4},
+        carInfo: {model: "model", make: "Toyota", number: "CXR004", seats: 4, freeSeats: 4},  
+    },
+    {
+        info: {avatarUrl: faker.image.avatar(), name: faker.name.firstName(), gender:"gender", from: "700 Markham Road", to: "25 York Street", workday:[1,2,4,5], eta: "9:10 am", rating: 5},
+        carInfo: {model: "model", make: "Honda", number: "XVE999", seats: 5, freeSeats: 2},  
+    },
+    {
+        info: {avatarUrl: faker.image.avatar(), name: faker.name.firstName(), gender:"gender", from: "900 Markham Road", to: "30 York Street", workday:[2,4,5], eta: "9:30 am", rating: 5},
+        carInfo: {model: "model", make: "Subaru", number: "VZP204", seats: 4, freeSeats: 3},  
+    },
+    {
+        info: {avatarUrl: faker.image.avatar(), name: faker.name.firstName(), gender:"gender", from: "900 Markham Road", to: "30 King Street", workday:[2,4,5,6,7], eta: "8:40 am", rating: 5},
+        carInfo: {model: "model", make: "Subaru", number: "VZP804", seats: 4, freeSeats: 1},  
+    }
+];
+
+const ResultList = () => {
     const [showFilter, setShowFilter] = useState(false);
+
 return (
     <>
         <button type="button" onClick={()=> setShowFilter(!showFilter)}>Filter Results</button>
@@ -99,67 +114,39 @@ return (
 
         <>
         <h4>Your Request</h4>
-        <div>
-            <p>
-                <img src="https://png.pngtree.com/png-vector/20190710/ourlarge/pngtree-user-vector-avatar-png-image_1541962.jpg" width="50px"/>
-            </p>
+        <ListItem>
+            <Avatar src={faker.image.avatar()} />
             <div>
                 <p>Name: Edwin</p>
                 <p>From: 1800 Richmond Hill</p>  
                 <p>To: 18 York Street</p>
             </div>
-        </div>
+        </ListItem>
 
         <h4>Available Rides</h4>
-        <div>
-            <p>
-                <img src="https://png.pngtree.com/png-vector/20190710/ourlarge/pngtree-user-vector-avatar-png-image_1541962.jpg" width="50px"/>
-            </p>
-            <div>
-                <p>Name: Edward</p>
-                <p>From: 1800 Markham Road</p>  
-                <p>To: 25 York Street</p>
-            </div>
-            <div>
-                <p>Driver's Rating: ****</p>
-                <p>Car Model: Toyota</p>  
-                <p>Number: CVR 001</p>
-            </div>
-            <button><Link to="/ride">Request</Link></button>
-        </div>
-        <div>
-            <p>
-                <img src="https://png.pngtree.com/png-vector/20190710/ourlarge/pngtree-user-vector-avatar-png-image_1541962.jpg" width="50px"/>
-            </p>
-            <div>
-                <p>Name: Peter</p>
-                <p>From: 1800 Richmond Hill</p>  
-                <p>To: 900 King Street</p>
-            </div>
-            <div>
-                <p>Driver's Rating: ****</p>
-                <p>Car Model: Honda</p>  
-                <p>Number: CVR 011</p>
-            </div>
-            <button><Link to="/ride">Request</Link></button>
-        </div>
-        <div>
-            <p>
-                <img src="https://png.pngtree.com/png-vector/20190710/ourlarge/pngtree-user-vector-avatar-png-image_1541962.jpg" width="50px"/>
-            </p>
-            <div>
-                <p>Name: Mary</p>
-                <p>From: North York</p>  
-                <p>To: 30 York Street</p>
-            </div>
-            <div>
-                <p>Driver's Rating: ****</p>
-                <p>Car Model: Toyota</p>  
-                <p>Number: CXV 111</p>
-            </div>
-            <button><Link to="/ride">Request</Link></button>
-        </div>
 
+        <List>
+        {
+            availableRides.map((ride, index) => {
+                return <ListItem key={index}>
+                    <Avatar src={ride.info.avatarUrl} />
+                    <div>
+                        <p>{ride.info.name}</p>
+                        <p>{ride.info.from}</p>  
+                        <p>{ride.info.to}</p>
+                        <p>{ride.info.workday}</p>
+                        <p>{ride.info.eta}</p>
+                    </div>
+                    <div>
+                        <p>{ride.carInfo.make}</p>
+                        <p>{ride.carInfo.number}</p>
+                        <p>{ride.carInfo.freeSeats} / {ride.carInfo.seats}</p>
+                    </div>
+                    <ListItemButton><Link to="/ride">Request</Link></ListItemButton>
+                </ListItem>
+            })
+        }
+        </List>
     </>
 
 </>
@@ -168,4 +155,4 @@ return (
 }
 
 
-export default List;
+export default ResultList;
