164ea836b890f2c6e5d331f101c4d25713507a0d
edwintsanghm <edwintsanghm@gmail.com>
2022-09-10 14:49:13 -0400

    add list & ride pages

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import { Routes, Route, Link } from "react-router-dom";
 import Landing from './component/Landing';
 import List from './component/List';
 import Form from './component/Form';
+import RideInfo from './component/RideInfo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/">Landing</Link>
+        <Link to="/">About</Link>
         <Link to="/form">Form</Link>
         <Link to="/list">List</Link>
+        <Link to="/ride">Ride</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="form" element={<Form />} />
         <Route path="list" element={<List />} />
+        <Route path="ride" element={<RideInfo />} />
       </Routes>
     </div>
   );
--- src/component/Landing.js
+++ src/component/Landing.js
@@ -1,4 +1,22 @@
+import { Link } from "react-router-dom";
 
-const Landing = () => <p>Landing</p>
+const Landing = () => (
+<>
+    <p>
+        <Link to="/form">
+            <button type="button">
+            I'm a Driver
+            </button>
+        </Link>
+    </p>
+    <p>
+        <Link to="/form">
+            <button type="button">
+            I'm a Passenger
+            </button>
+        </Link>
+    </p>
+</>
+)
 
 export default Landing;
